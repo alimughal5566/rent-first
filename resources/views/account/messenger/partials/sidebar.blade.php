0639c254186f8f8d4bd9cb1266f3fdc9cad991f9
shaji haider <shaji.haider@ivylabtech.com>
2021-02-19 18:02:28 +0500

    Initial commit

---
+++ resources/views/account/messenger/partials/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="col-md-3 col-lg-2">
+	<ul class="nav nav-pills inbox-nav">
+		<li class="nav-item{{ (!request()->has('filter') || request()->get('filter')=='') ? ' active' : '' }}">
+			<a class="nav-link" href="{{ url('account/messages') }}">
+				{{ t('inbox') }}
+				<?php
+				$badgeColor = (!request()->has('filter') || request()->get('filter')=='') ? 'badge-light' : 'badge-primary text-white';
+				$visibility = (!isset($countThreadsWithNewMessage) || $countThreadsWithNewMessage <= 0) ? ' hide' : '';
+				?>
+			</a>
+		</li>
+		<li class="nav-item{{ (request()->get('filter')=='unread') ? ' active' : '' }}">
+			<a class="nav-link" href="{{ url('account/messages?filter=unread') }}">
+				{{t('unread')}}
+
+				
+			</a>
+		</li>
+		
+		<li class="nav-item{{ (request()->get('filter')=='started') ? ' active' : '' }}">
+			<a class="nav-link" href="{{ url('account/messages?filter=started') }}">
+				{{ t('started') }}
+			</a>
+		</li>
+		<li class="nav-item{{ (request()->get('filter')=='important') ? ' active' : '' }}">
+			<a class="nav-link" href="{{ url('account/messages?filter=important') }}">
+				{{ t('important') }}
+			</a>
+		</li>
+		<li class="nav-item{{ (request()->get('filter')=='request') ? ' active' : '' }}">
+			<a class="nav-link" href="{{ url('account/messages?filter=request') }}">
+				Message Request
+
+				<span class="count-threads-with-new-messages count badge {{ $badgeColor }}{{ $visibility }}">
+					{{ \App\Helpers\Number::short($countThreadsWithNewMessage) }}
+				</span>
+			</a>
+		</li>
+	</ul>
+</div>
